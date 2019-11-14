109ef20d53082d36a446d3924372cf3e6388b686
abanoubmesiha <bony.nagy@gmail.com>
2019-11-14 16:41:16 +0200

    website answer

--- 7-string_parameterize.js
+++ 7-string_parameterize.js
@@ -1,17 +1,6 @@
-function string_parameterize(str){
-    if (typeof str == 'string'){
-        let strToArr = str.split(" ");
-        console.log(strToArr);
-        let i;
-        var newArr = [];
-       for (i=0;i<strToArr.length;i++){
-             newArr[newArr.length] = strToArr[i];
-            }
-            let joinedStr = newArr.join('-');
-            let result = joinedStr.toLocaleLowerCase();
-            console.log(result.replace(/\./g,''));
-        }
-}
+string_parameterize = function (str1) {
+    return str1.trim().toLowerCase().replace(/[^a-zA-Z0-9 -]/, "").replace(/\s/g, "-");
+};
 
-string_parameterize("Robin Singh from USA.");
+console.log(string_parameterize("Robin Singh from USA."));
 //robin-singh-from-usa
